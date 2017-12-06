f002d5664f59daf8f00cc01a8b991852960e574f
Logan Herrmeyer <logern5@github.com>
2017-12-06 10:01:19 -0600

    Adding license info

--- CCurl_simple.c
+++ CCurl_simple.c
@@ -7,13 +7,13 @@
  *
  * Copyright (C) 1998 - 2015, Daniel Stenberg, <daniel@haxx.se>, et al.
  *
- * This software is licensed as described in the file COPYING, which
+ * This software is licensed as described in the file LIBCURL_COPYING, which
  * you should have received as part of this distribution. The terms
  * are also available at https://curl.haxx.se/docs/copyright.html.
  *
  * You may opt to use, copy, modify, merge, publish, distribute and/or sell
  * copies of the Software, and permit persons to whom the Software is
- * furnished to do so, under the terms of the COPYING file.
+ * furnished to do so, under the terms of the LIBCURL_COPYING file.
  *
  * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
  * KIND, either express or implied.
